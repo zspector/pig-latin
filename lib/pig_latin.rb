599f3763445f9803e305b515363428c577fc0e3d
Zachary Spector <zjspector@gmail.com>
2014-05-07 16:27:20 -0500

    Write tests and implement code for translate. Refactor capture_back

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -3,7 +3,9 @@ require "pig_latin/version"
 module PigLatin
   # Your code goes here...
   def self.cut_front(word)
-    if /\Ay/.match(word).to_s == "y" || /y/.match(word) == nil
+    if /\A[aeiou]/.match(word) != nil
+      return "w"
+    elsif /\Ay/.match(word).to_s == "y" || /y/.match(word) == nil
       /[^aeiou]+/.match(word).to_s
     else
       /[^aeiouy]+/.match(word).to_s
@@ -11,10 +13,14 @@ module PigLatin
   end
 
   def self.capture_back(word)
-    if /\Ay/.match(word).to_s == "y" || /y/.match(word) == nil
-      /[aeiou](.*)/.match(word).to_s
-    else
-      /[aeiouy](.*)/.match(word).to_s
-    end
+    /\Ay/.match(word).to_s == "y" || /y/.match(word) == nil ? /[aeiou](.*)/.match(word).to_s : /[aeiouy](.*)/.match(word).to_s
+    #   /[aeiou](.*)/.match(word).to_s
+    # else
+    #   /[aeiouy](.*)/.match(word).to_s
+    # end
+  end
+
+  def self.translate(word)
+    "#{capture_back(word)}-#{cut_front(word)}ay"
   end
 end
--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -19,6 +19,10 @@ describe 'PigLatin' do
       expect(PigLatin.cut_front("rhythm")).to eq("rh")
       expect(PigLatin.cut_front("harvey")).to eq("h")
     end
+
+    it "returns 'way' if the word starts with a vowel" do
+      expect(PigLatin.cut_front("owl")).to eq("w")
+    end
   end
 
   describe '.capture_back' do
@@ -41,6 +45,33 @@ describe 'PigLatin' do
 
     it "captures words with two vowels in a row" do
       expect(PigLatin.capture_back("reek")).to eq ("eek")
+    end
+  end
+
+  describe '.translate' do
+    it "returns a simple word in pig latin" do
+      expect(PigLatin.translate("bad")).to eq("ad-bay")
+    end
+
+    it "translates a word with multiple consonants in front to pig latin" do
+      expect(PigLatin.translate("flow")).to eq("ow-flay")
+    end
+
+    it "translates words beginning with 'y' correctly" do
+      expect(PigLatin.translate("yellow")).to eq("ellow-yay")
+    end
+
+    it "translates words containing but not starting with 'y' correctly" do
+      expect(PigLatin.translate("rhythm")).to eq("ythm-rhay")
+      expect(PigLatin.translate("harvey")).to eq("arvey-hay")
+    end
+
+    it "translates words with only 'y' as a vowel correctly" do
+      expect(PigLatin.translate("my")).to eq("y-may")
+    end
+
+    it "translates words beginning with vowels correctly" do
+      expect(PigLatin.translate("aardvark")).to eq("aardvark-way")
     end
   end
 end
