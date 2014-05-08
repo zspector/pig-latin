0da1f726a0df4bf9f6d7625db1dad43e4d018ea0
Zachary Spector <zjspector@gmail.com>
2014-05-08 16:57:04 -0500

    Change code so it is case insensitive

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -3,17 +3,17 @@ require "pig_latin/version"
 module PigLatin
   # Your code goes here...
   def self.cut_front(word)
-    if /\A[aeiou]/.match(word) != nil
+    if /\A[aeiou]/i.match(word) != nil
       return "w"
-    elsif /\Ay/.match(word).to_s == "y" || /y/.match(word) == nil
-      /[^aeiou]+/.match(word).to_s
+    elsif /\Ay/i.match(word).to_s == "y" || /y/i.match(word) == nil
+      /[^aeiou]+/i.match(word).to_s
     else
-      /[^aeiouy]+/.match(word).to_s
+      /[^aeiouy]+/i.match(word).to_s
     end
   end
 
   def self.capture_back(word)
-    /\Ay/.match(word).to_s == "y" || /y/.match(word) == nil ? /[aeiou](.*)/.match(word).to_s : /[aeiouy](.*)/.match(word).to_s
+    /\Ay/i.match(word).to_s == "y" || /y/i.match(word) == nil ? /[aeiou](.*)/.match(word).to_s : /[aeiouy](.*)/.match(word).to_s
   end
 
   def self.translate(word)
